10f11c6cb30763fc080a78c7b08db72d2ae9dc7d
tj--- <trilokj@gmail.com>
2021-05-23 17:10:05 +0530

    Doc update: Setting the correct cron type

--- src/main/scala/io/hevo/scheduler/dto/task/CronTask.scala
+++ src/main/scala/io/hevo/scheduler/dto/task/CronTask.scala
@@ -3,11 +3,11 @@ package io.hevo.scheduler.dto.task
 import io.hevo.scheduler.core.Constants
 
 /**
- * A Schedulable task that executes based on a (Unix) Cron schedule
+ * A Schedulable task that executes based on a (Quartz) Cron schedule
  *
  * @param _nameSpace Namespace (Logical grouping) for the task. May be left blank
  * @param _key Unique identifier of a task.
- * @param _cronExpression Cron (Unix) expression
+ * @param _cronExpression Cron (Quartz) expression
  * @param _handlerFqcn Fully Qualified Class Name of the handler class
  * parameters: Execution context that would be passed as is to the Handler class
  */
