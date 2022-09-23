51ef29b22805da1c10eaa9a42a28bf264c2f15b8
Pratyush Chandra <pratyush.chandra@hevodata.com>
2022-09-24 02:06:43 +0530

    Add replication instrumentation

--- src/main/scala/com/hevodata/scheduler/dto/ExecutionContext.scala
+++ src/main/scala/com/hevodata/scheduler/dto/ExecutionContext.scala
@@ -1,11 +1,14 @@
 package com.hevodata.scheduler.dto
 
+import java.util.Date
+
 /**
  * ToDo: Support the parameters as a Map
+ *
  * @param parameters required by the Job execution. It can be a JSON serialized parameters map.
  */
-case class ExecutionContext(parameters: String) {
+case class ExecutionContext(parameters: String, nextExecutionTime: Date) {
   def this() {
-    this(null)
+    this(null, null)
   }
 }
